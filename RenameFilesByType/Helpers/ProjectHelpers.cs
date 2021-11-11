b39c3c1895b33911f079dfaf538e221d8add994a
Prasanth Reddy M <prasanthreddy.maddala@gmail.com>
2021-11-11 19:13:24 +0530

    Added Rename Files By Type Functionality

---
+++ RenameFilesByType/Helpers/ProjectHelpers.cs
@@ -0,0 +1,62 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenameFilesByType.Helpers
+{
+    internal static class ProjectHelpers
+    {
+        static DTE2 _dte = RenameFilesByType._dte;
+
+        public static bool IsKind(this Project project, params string[] kindGuids)
+        {
+            foreach (string guid in kindGuids)
+            {
+                if (project.Kind.Equals(guid, StringComparison.OrdinalIgnoreCase))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static object GetSelectedItem()
+        {
+            object selectedObject = null;
+
+            var monitorSelection = (IVsMonitorSelection)Package.GetGlobalService(typeof(SVsShellMonitorSelection));
+
+            try
+            {
+                monitorSelection.GetCurrentSelection(out IntPtr hierarchyPointer,
+                                                 out uint itemId,
+                                                 out IVsMultiItemSelect multiItemSelect,
+                                                 out IntPtr selectionContainerPointer);
+
+
+                if (Marshal.GetTypedObjectForIUnknown(
+                                                     hierarchyPointer,
+                                                     typeof(IVsHierarchy)) is IVsHierarchy selectedHierarchy)
+                {
+                    ErrorHandler.ThrowOnFailure(selectedHierarchy.GetProperty(itemId, (int)__VSHPROPID.VSHPROPID_ExtObject, out selectedObject));
+                }
+
+                Marshal.Release(hierarchyPointer);
+                Marshal.Release(selectionContainerPointer);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex);
+            }
+
+            return selectedObject;
+        }
+    }
+}
--- RenameFilesByType/RenameFilesByType.cs
+++ RenameFilesByType/RenameFilesByType.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.Shell;
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using System;
 using System.ComponentModel.Design;
@@ -6,6 +7,13 @@ using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
 using Task = System.Threading.Tasks.Task;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using EnvDTE80;
+using RenameFilesByType.Helpers;
 
 namespace RenameFilesByType
 {
@@ -28,6 +36,8 @@ namespace RenameFilesByType
         /// VS Package that provides this command, not null.
         /// </summary>
         private readonly AsyncPackage package;
+
+        public static DTE2 _dte;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RenameFilesByType"/> class.
@@ -75,6 +85,8 @@ namespace RenameFilesByType
             // the UI thread.
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
 
+            _dte = await package.GetServiceAsync(typeof(DTE)) as DTE2;
+
             OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
             Instance = new RenameFilesByType(package, commandService);
         }
@@ -88,18 +100,112 @@ namespace RenameFilesByType
         /// <param name="e">Event args.</param>
         private void Execute(object sender, EventArgs e)
         {
-            ThreadHelper.ThrowIfNotOnUIThread();
-            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
-            string title = "RenameFilesByType";
-
-            // Show a message box to prove we were here
-            VsShellUtilities.ShowMessageBox(
-                this.package,
-                message,
-                title,
-                OLEMSGICON.OLEMSGICON_INFO,
-                OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            object item = ProjectHelpers.GetSelectedItem();
+            string[] allFiles = FindAllFilesInSelectedPath(item);
+            RenameAllFilesByType(allFiles);
         }
+
+        private static void RenameAllFilesByType(string[] allFiles)
+        {
+            if (allFiles.Length == 0)
+                return;
+
+            foreach (string file in allFiles)
+            {
+                RenameFileByType(file);
+            }
+        }
+
+        private static void RenameFileByType(string file)
+        {
+            var fileCode = File.ReadAllText(file);
+            var tree = CSharpSyntaxTree.ParseText(fileCode);
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+
+            List<BaseTypeDeclarationSyntax> typesInSourceCode = GetAllTypesInSourceCode(root);
+
+            if (!typesInSourceCode.Any())
+                return;
+
+            if (typesInSourceCode.Count() > 1)
+                return;
+
+            System.IO.FileInfo fi = new System.IO.FileInfo(file);
+            if (fi.Exists)
+            {
+                var sourceType = typesInSourceCode.Single();
+                try
+                {
+                    var typeName = GetTypeNameFromSourceType(sourceType);
+                    try
+                    {
+                        fi.MoveTo(Path.Combine(fi.DirectoryName, typeName + fi.Extension));
+                    }
+                    catch(Exception)
+                    {
+                        fi.MoveTo(Path.Combine(fi.DirectoryName, typeName+ "."+ Guid.NewGuid().ToString().Substring(0,3) + fi.Extension));
+                    }
+                }
+                catch (InvalidDataException)
+                {
+
+                }
+            }
+        }
+
+        private static List<BaseTypeDeclarationSyntax> GetAllTypesInSourceCode(CompilationUnitSyntax root)
+        {
+            List<BaseTypeDeclarationSyntax> types = new List<BaseTypeDeclarationSyntax>();
+
+            var classes = root.DescendantNodes().OfType<ClassDeclarationSyntax>();
+            var enums = root.DescendantNodes().OfType<EnumDeclarationSyntax>();
+            var interfaces = root.DescendantNodes().OfType<InterfaceDeclarationSyntax>();
+            var structs = root.DescendantNodes().OfType<StructDeclarationSyntax>();
+            types.AddRange(classes);
+            types.AddRange(enums);
+            types.AddRange(interfaces);
+            types.AddRange(structs);
+
+            return types;
+        }
+
+        private static string GetTypeNameFromSourceType(object obj)
+        {
+            switch (obj)
+            {
+                case ClassDeclarationSyntax _:
+                    return ((ClassDeclarationSyntax)obj).Identifier.Text;
+                case EnumDeclarationSyntax _:
+                    return ((EnumDeclarationSyntax)obj).Identifier.Text;
+                case InterfaceDeclarationSyntax _:
+                    return ((InterfaceDeclarationSyntax)obj).Identifier.Text;
+                case StructDeclarationSyntax _:
+                    return ((StructDeclarationSyntax)obj).Identifier.Text;
+                default:
+                    throw new InvalidDataException();
+            }
+        }
+
+        private static string[] FindAllFilesInSelectedPath(object item)
+        {
+            if (item == null)
+                return null;
+
+            var projectItem = item as ProjectItem;
+
+            if (projectItem == null)
+                return new string[] { };
+
+            string fileName = projectItem.FileNames[1];
+            if (File.Exists(fileName))
+            {
+                return new string[] { fileName };
+            }
+            else
+            {
+                return Directory.GetFiles(fileName, "*", SearchOption.AllDirectories);
+            }
+        }
+
     }
 }
