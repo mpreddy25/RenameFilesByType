ae080a51a495c69bb394aa6196d7c12ea78b0e56
Prasanth Reddy M <prasanthreddy.maddala@gmail.com>
2021-11-11 18:13:29 +0530

    Added Command in Folder Explore

--- RenameFilesByType/RenameFilesByTypePackage.cs
+++ RenameFilesByType/RenameFilesByTypePackage.cs
@@ -25,6 +25,7 @@ namespace RenameFilesByType
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(RenameFilesByTypePackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class RenameFilesByTypePackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace RenameFilesByType
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await RenameFilesByType.InitializeAsync(this);
         }
 
         #endregion
